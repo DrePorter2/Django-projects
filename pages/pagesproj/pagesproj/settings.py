c677bce99e493e99c60e20d8576bffaaeced9106
DrePorter2 <deondre2016porter@gmail.com>
2022-09-08 01:37:55 -0700

    Update settings.py

--- pages/pagesproj/pagesproj/settings.py
+++ pages/pagesproj/pagesproj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=+4hl_@zb&&-!#ypy3_1*e3v-l*@4gix3ld4i6(2%j@teqs9)u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
