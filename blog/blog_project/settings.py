9f8b34d4e4cb38177a92f64fe7dd3a0ebdf4d1ae
DrePorter2 <deondre2016porter@gmail.com>
2022-09-09 04:39:10 -0700

    CRUD features & Login/Logout func.

--- blog/blog_project/settings.py
+++ blog/blog_project/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',  # new
+    'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',  # new
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +125,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
